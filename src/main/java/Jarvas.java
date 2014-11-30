344cff7ff8741648b7a75ae3659329fae9c94855
Maria João <mjaraujosoutelo@gmail.com>
2014-11-30 21:01:16 +0000

    Weather and Traffic update functions.

--- src/main/java/Jarvas.java
+++ src/main/java/Jarvas.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import jadex.bdiv3.annotation.Belief;
 import jadex.micro.annotation.Agent;
 import jadex.micro.annotation.AgentBody;
@@ -7,13 +9,13 @@ import jadex.micro.annotation.AgentBody;
  */
 @Agent
 public class Jarvas {
-	
+
 	private WorldMap map;
 	private String timePeriod;
-	private long currentTime; 
+	private long currentTime;
 	private String weather;
 	private String traffic;
-	
+
 	/**
 	 * Called when the agent is started.
 	 */
@@ -22,96 +24,151 @@ public class Jarvas {
 		System.out.println("Hello world!");
 	}
 
-	
 	/* Time Period - "Day" or "Night" */
 
 	@Belief
-	public String getTimePeriod()
-	{
-	  return timePeriod;
+	public String getTimePeriod() {
+		return timePeriod;
 	}
 
 	@Belief
-	public void setTimePeriod(String timePeriod)
-	{
-	  this.timePeriod = timePeriod;
+	public void setTimePeriod(String timePeriod) {
+		this.timePeriod = timePeriod;
 	}
-
 
 	/* Current Time */
 
 	@Belief
-	public long getCurrentTime()
-	{
-	  return currentTime;
+	public long getCurrentTime() {
+		return currentTime;
 	}
 
 	@Belief
-	public void setCurrentTime(long currentTime)
-	{
-	  this.currentTime = currentTime;
+	public void setCurrentTime(long currentTime) {
+		this.currentTime = currentTime;
 	}
-
 
 	/* Weather */
 
-	@Belief
-	public String getWeather()
-	{
-		// TODO -> update weather???
+	public boolean updateWeather() {
+		
+		Random rand = new Random();
 
-	  return weather;
+		// 100 is the maximum and 1 is the minimum
+		int n = rand.nextInt(100) + 1;
+
+		// Weather Probability
+		int weatherP = 1 / n;
+
+		boolean weatherUpdated = false;
+
+		if (weatherP < 0) {
+			return weatherUpdated;
+		} else if (weatherP < 0.1) {
+			this.weather = "Snow";
+			weatherUpdated = true;
+		}else if (weatherP < 0.2) {
+			this.weather = "Hail";
+			weatherUpdated = true;
+		} else if (weatherP < 0.4) {
+			this.weather = "HighRain";
+			weatherUpdated = true;
+		} else if (weatherP < 0.6) {
+			this.weather = "LightRain";
+			weatherUpdated = true;
+		} else if (weatherP < 0.8) {
+			this.weather = "Cloudy";
+			weatherUpdated = true;
+		} else if (weatherP < 1) {
+			this.weather = "Sunny";
+			weatherUpdated = true;
+		}
+
+		return weatherUpdated;
 	}
 
 	@Belief
-	public void setWeather(String weather)
-	{
-	  this.weather = weather;
+	public String getWeather() {
+		return this.weather;
 	}
 
+	@Belief
+	public void setWeather(String weather) {
+		this.weather = weather;
+	}
+
+	/* Traffic */
+
+	public boolean updateTraffic() {
+		Random rand = new Random();
+
+		// 100 is the maximum and 1 is the minimum
+		int n = rand.nextInt(100) + 1;
+
+		// Traffic Probability
+		int trafficP = 1 / n;
+
+		boolean trafficUpdated = false;
+
+		if (trafficP < 0) {
+			return trafficUpdated;
+		} else if (trafficP < 0.2) {
+			this.traffic = "None";
+			trafficUpdated = true;
+		} else if (trafficP < 0.4) {
+			this.traffic = "Light";
+			trafficUpdated = true;
+		} else if (trafficP < 0.6) {
+			this.traffic = "Moderate";
+			trafficUpdated = true;
+		} else if (trafficP < 0.8) {
+			this.traffic = "High";
+			trafficUpdated = true;
+		} else if (trafficP < 1) {
+			this.traffic = "Stopped";
+			trafficUpdated = true;
+		}
+
+		return trafficUpdated;
+	}
 
 	/* Traffic */
 
 	@Belief
-	public String getTraffic()
-	{
-		// TODO -> update traffic???
-
-	  return traffic;
+	public String getTraffic() {
+		return this.traffic;
 	}
 
 	@Belief
-	public void setTraffic(String traffic)
-	{
-	  this.traffic = traffic;
+	public void setTraffic(String traffic) {
+		this.traffic = traffic;
 	}
 
 	/* Sightseeing Locations */
 
 	@Belief
-	public boolean isSightseeingLocation(int xPos1, int yPos1, int xPos2, int yPos2)
-	{
-	  boolean isSightseeingLoc = false;
-		
-	  Node n1 = new Node(xPos1, yPos1);
-	  Node n2 = new Node(xPos2, yPos2);
-	  
-	  if(this.map.isSightseeingLocation(n1, n2) == true)
-	  {
-		  isSightseeingLoc = true;
-	  }
+	public boolean isSightseeingLocation(int xPos1, int yPos1, int xPos2,
+			int yPos2) {
+		boolean isSightseeingLoc = false;
 
-	  return isSightseeingLoc;
+		Node n1 = new Node(xPos1, yPos1);
+		Node n2 = new Node(xPos2, yPos2);
+
+		if (this.map.isSightseeingLocation(n1, n2) == true) {
+			isSightseeingLoc = true;
+		}
+
+		return isSightseeingLoc;
 	}
 
 	@Belief
-	public void setSightseeingLocation(int xPos1, int yPos1, int xPos2, int yPos2)
-	{
-	   Node n1 = new Node(xPos1, yPos1);
-	   Node n2 = new Node(xPos2, yPos2);
-	  
-	  this.map.setSightseeingLocation(n1, n2);
-	  
+	public void setSightseeingLocation(int xPos1, int yPos1, int xPos2,
+			int yPos2) {
+		Node n1 = new Node(xPos1, yPos1);
+		Node n2 = new Node(xPos2, yPos2);
+
+		this.map.setSightseeingLocation(n1, n2);
+
 	}
-	
+
 }
